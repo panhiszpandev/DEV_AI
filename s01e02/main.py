5ad103734658aec27ba628a7e829709b09b3e982
Arkadiusz Mazur <210825596+panhiszpandev@users.noreply.github.com>
2026-03-11 20:33:40 +0100

    Add suspect location and access level fetching

--- s01e02/main.py
+++ s01e02/main.py
@@ -16,9 +16,37 @@ def fetch_power_plants() -> list:
     return [{"city": city, **info} for city, info in plants.items()]
 
 
+def fetch_locations(name: str, surname: str) -> list:
+    response = requests.post(
+        "https://hub.ag3nts.org/api/location",
+        json={"apikey": HUB_KEY, "name": name, "surname": surname},
+    )
+    return response.json()
+
+
+def fetch_access_level(name: str, surname: str, birth_year: int) -> int:
+    response = requests.post(
+        "https://hub.ag3nts.org/api/accesslevel",
+        json={"apikey": HUB_KEY, "name": name, "surname": surname, "birthYear": birth_year},
+    )
+    return response.json()
+
+
 def main():
     # 1. Fetch power plants
     power_plants = fetch_power_plants()
+
+    # 2. Load suspects from s01e01
+    with open(SUSPECTS_PATH) as f:
+        suspects = json.load(f)
+
+    # 3. Fetch locations and access level for each suspect
+    suspects_with_data = []
+    for person in suspects:
+        locations = fetch_locations(person["name"], person["surname"])
+        access_level = fetch_access_level(person["name"], person["surname"], person["born"])
+        suspects_with_data.append({**person, "locations": locations, "accessLevel": access_level})
+        print(f"{person['name']} {person['surname']}: locations={locations}, accessLevel={access_level}")
 
 
 if __name__ == "__main__":
